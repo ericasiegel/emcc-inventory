61673ff985c6c91f635cb91a5ea95fea93cb39dd
Erica Siegel <siegel.erica@gmail.com>
2023-08-31 12:39:11 -0700

    made changes to CookieGrid

--- client/src/components/CookieGrid.tsx
+++ client/src/components/CookieGrid.tsx
@@ -13,8 +13,8 @@ const CookieGrid = ({ activeCookie }: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   if (error) return <Text>{error}</Text>
+
   return (
-    <>
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={3}
@@ -32,7 +32,6 @@ const CookieGrid = ({ activeCookie }: Props) => {
           </CookieCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 };
 
