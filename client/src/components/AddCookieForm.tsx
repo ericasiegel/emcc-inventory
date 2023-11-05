720e266f45a814c635179e8a02e3afc9f2e4e1fb
Erica Siegel <siegel.erica@gmail.com>
2023-11-04 18:11:10 -0700

    took checkbox away from add cookie form

--- client/src/components/AddCookieForm.tsx
+++ client/src/components/AddCookieForm.tsx
@@ -5,9 +5,7 @@ import {
   Box,
   Button,
   Center,
-  Checkbox,
   FormControl,
-  // FormLabel,
   Input,
 } from "@chakra-ui/react";
 import { AddUpdateCookie } from "../entities/Cookie";
@@ -15,35 +13,26 @@ import useAddCookie from "../hooks/useAddCookie";
 
 const AddCookieForm = () => {
 
-  const [activeCookie, setActiveCookie] = useState<boolean>(true);
   const [name, setName] = useState<string>("");
   const resetForm = () => {
     setName("");
-    setActiveCookie(true);
   };
 
   const { addCookie, error, isLoading } = useAddCookie(resetForm)
 
-
   const cookieName = useRef<HTMLInputElement>(null);
-  const isActive = useRef<HTMLInputElement>(null);
 
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    // Extract the checked state of the Checkbox and convert it to a boolean
-    const isActiveValue = isActive.current?.checked || false;
-
-    // Ensure that cookieName.current?.value is not undefined
     const nameValue = cookieName.current?.value || "";
 
     const cookieData: AddUpdateCookie = {
       name: nameValue,
-      is_active: isActiveValue,
+      is_active: true,
     };
 
     addCookie(cookieData);
-    // resetForm();
   };
 
 
@@ -65,17 +54,6 @@ const AddCookieForm = () => {
               value={name} // Set the input value from formData
               onChange={(e) => setName(e.target.value)} // Update formData on input change
             />
-            <Checkbox
-              paddingTop={3}
-              ref={isActive}
-              defaultChecked
-              checked={activeCookie} // Set the checked state from formData
-              onChange={(e) => {
-                setActiveCookie(e.target.checked); // Update the separate state for the checkbox
-              }}
-            >
-              Active Cookie?
-            </Checkbox>
           </Box>
           <Center>
             <Button
