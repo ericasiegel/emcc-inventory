ac7ab380072f4f4dff032d9aafdd0b417e5a008e
Erica Siegel <siegel.erica@gmail.com>
2024-03-28 11:30:48 -0700

    created form to add Location to sidebar

--- client/src/location/LocationCard.tsx
+++ client/src/location/LocationCard.tsx
@@ -12,8 +12,13 @@ import {
 import useGetData from "../hooks/useGetData";
 import { Location } from "./Location";
 import { LOCATIONS_ENDOINT } from "../constants";
+import AddLocationForm from "./AddLocationForm";
+import { useState } from "react";
+import AddButton from "../components/AddButton";
 
 const LocationCard = () => {
+  const [openForm, setOpenForm] = useState(false);
+
   const {
     data: getLocations,
     isLoading,
@@ -33,16 +38,33 @@ const LocationCard = () => {
     );
 
   return (
-    <Container color="#941c3e" width="100%" padding={4} borderTop='4px' borderBottom='4px'>
+    <Container
+      color="#941c3e"
+      width="100%"
+      padding={4}
+      borderTop="4px"
+      borderBottom="4px"
+    >
       <Center paddingBottom={3}>
         <Heading fontSize="3xl">Locations</Heading>
+      </Center>
+      <Center>
+        {openForm ? (
+          <AddLocationForm closeForm={() => setOpenForm(false)} />
+        ) : (
+          <AddButton onClick={() => setOpenForm(true)} />
+        )}
       </Center>
       {isLoading && <Spinner />}
       <UnorderedList>
         {locations?.map((location) => (
-          <ListItem key={location.id}><Text fontSize='xl'>{location.title}</Text></ListItem>
+          <ListItem key={location.id}>
+            <Text fontSize="xl">{location.title}</Text>
+            {/* add delete button */}
+          </ListItem>
         ))}
       </UnorderedList>
+      {/* add Alert box for reason why it can't be deleted if there are items present */}
     </Container>
   );
 };
