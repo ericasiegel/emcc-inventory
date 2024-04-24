e47c746222f45cb18b1e3d8625d60c31ad58211b
Erica Siegel <siegel.erica@gmail.com>
2024-04-24 14:19:43 -0700

    logout function created

--- client/src/userAuth/LogoutButton.tsx
+++ client/src/userAuth/LogoutButton.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@chakra-ui/react";
 
 const LogoutButton = () => {
+    const handleLogout = () => {
+        localStorage.removeItem('accessToken');
+        window.location.reload();
+    }
+
   return (
-    <Button colorScheme="black" size="lg" variant="link">
+    <Button colorScheme="black" size="lg" variant="link" onClick={handleLogout}>
       Log Out
     </Button>
   );
