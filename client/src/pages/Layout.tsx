1f1f7468cd32358c8e95a8bab9a15f7680c9908c
Erica Siegel <siegel.erica@gmail.com>
2024-04-24 13:40:06 -0700

    logic added for LoginModal to open when not logged in

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,8 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useDisclosure } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import MainModal from "../components/MainModal";
+import LoginForm from "../userAuth/LoginForm";
 
 const Layout = () => {
+  const [ isLoggedIn, setIsLoggedIn] = useState(false);
+  const { isOpen, onClose } = useDisclosure()
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (token) setIsLoggedIn(true);
+  }, []);
+
   const containerStyle: React.CSSProperties = {
     backgroundColor: "#f8a5c2", // Replace 'blue' with the color you want
     minHeight: "100vh", // Ensures the background covers the entire viewport
@@ -11,7 +22,13 @@ const Layout = () => {
     <div style={containerStyle}>
       <NavBar />
       <Box padding={5}>
-        <Outlet />
+        { isLoggedIn ? (
+          <Outlet />
+        ) : (
+          <MainModal header="Login to EMCC Inventory" isOpen={!isLoggedIn && !localStorage.getItem('accessToken')} onClose={onClose}>
+            <LoginForm />
+          </MainModal>
+        )}
       </Box>
     </div>
   );
