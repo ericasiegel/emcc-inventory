b46e7b79e88d941f12aabfe2d4fd8c8c6176d8e5
Erica Siegel <siegel.erica@gmail.com>
2024-04-24 13:09:24 -0700

    created method to fecth the berrer token from auth endpoint

--- client/src/pages/Pantry.tsx
+++ client/src/pages/Pantry.tsx
@@ -1,12 +1,11 @@
 
 import IngredientCard from '../pantry/IngredientCard'
-import LoginForm from '../userAuth/LoginForm'
+
 
 const Pantry = () => {
   return (
     <div>
       <IngredientCard />
-      <LoginForm />
     </div>
   )
 }
--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -8,8 +8,10 @@ export interface FetchResponse<T> {
     results: T[];
   }
 
-// const apiToken = localStorage.getItem('apiToken')
-const apiToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyMDYxNTI4LCJpYXQiOjE3MDE5NzUxMjgsImp0aSI6IjUyOWU3NGE5ZDY1OTRiM2ZiMmI2M2I3MGNmMTEzYWEyIiwidXNlcl9pZCI6MX0.GOHNhipE64AiSImnvpwgUXNCrfra3OZhx61dWj19Yqs'
+const apiToken = localStorage.getItem('apiToken')
+console.log(apiToken);
+
+// const apiToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyMDYxNTI4LCJpYXQiOjE3MDE5NzUxMjgsImp0aSI6IjUyOWU3NGE5ZDY1OTRiM2ZiMmI2M2I3MGNmMTEzYWEyIiwidXNlcl9pZCI6MX0.GOHNhipE64AiSImnvpwgUXNCrfra3OZhx61dWj19Yqs'
 
 const axiosInstance = axios.create({
     baseURL: 'http://127.0.0.1:8000/bakery',
@@ -83,6 +85,13 @@ class APIClient<T> {
             })
     }
 
+    fetchBearerToken = async (username: string, password: string) => {
+        const response = await axios.post('http://127.0.0.1:8000/auth/jwt/create/', {
+            username,
+            password,
+        });
+        return response.data.access;
+    };
 }
 
 export default APIClient
